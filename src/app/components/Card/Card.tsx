c2b3963e5857d6171fe22b64be818095251163b0
Olga Smith <ms.olgasmith.jj@mail.ru>
2024-02-25 21:55:41 +0300

    add toggle likes

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -2,7 +2,7 @@
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import styles from './Card.module.scss';
 import Image from 'next/image';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { fetchApiUsers } from '@/lib/features/userList';
 import React from 'react';
 import { useRouter } from 'next/navigation';
@@ -14,6 +14,9 @@ interface CardProps {
 }
 
 const Card: FC<CardProps> = (props) => {
+
+  const [likedCards, setLikedCards] = useState<{ [id: number]: boolean }>({});
+  
   const dispatch = useAppDispatch();
   const apiListData = useAppSelector((state) => state.userList.usersData);
   const route = useRouter();
@@ -26,6 +29,13 @@ const Card: FC<CardProps> = (props) => {
     route.push(`/pages/about/${id}`)
 };
 
+const handleAddFavorite = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, id: number) => {
+  e.stopPropagation()
+  setLikedCards((likedCards) => ({
+    ...likedCards,
+    [id]: !likedCards[id],
+  }));
+}
   return (
     <>
       {apiListData &&
@@ -42,7 +52,7 @@ const Card: FC<CardProps> = (props) => {
               <h2 className={styles.card__name}>
                 {item.first_name} {item.last_name}
               </h2>
-              <div className={styles.card__favorite}></div>
+              <div onClick={(e) => handleAddFavorite(e, item.id)} className={`${styles.card__favorite} ${likedCards[item.id] ? styles.card__favorite_active : ''}`}></div>
             </div>
           </div>
         ))}
