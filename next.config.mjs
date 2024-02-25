a13074e39148e15ad0cd67069131527c2d8e636d
Olga Smith <ms.olgasmith.jj@mail.ru>
2024-02-25 22:10:15 +0300

    config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/nextjs-github-pages',
   images: {
-    domains: ['reqres.in'], 
+    domains: ['reqres.in'],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'assets.example.com',
         port: '',
-        pathname: '/account123/**',
-      },
-    ],
-  },
+        pathname: '/account123/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
-
